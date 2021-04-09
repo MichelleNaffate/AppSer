2e8cb478f6808b171a0c60b827339c0444d17f8b
AnaCastilloMartinez <ana.castillo.itson@gmail.com>
2021-04-09 00:15:40 -0700

    Creación y actualización de HacerEjercicioActivity.

---
+++ app/src/main/java/castillo/holguin/naffate/ser/HacerEjercicioActivity.kt
@@ -0,0 +1,11 @@
+package castillo.holguin.naffate.ser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HacerEjercicioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hacer_ejercicio)
+    }
+}
