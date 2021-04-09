f27a12a3bcd870ca4105a29b18e1c9a69d2b0e19
Michelle Naffate <michelle.naffate205164@potros.itson.edu.mx>
2021-04-08 21:00:18 -0700

    Se modificaron detalles

---
+++ app/src/main/java/castillo/holguin/naffate/ser/CatalogoActividades.kt
@@ -0,0 +1,11 @@
+package castillo.holguin.naffate.ser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CatalogoActividades : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catalogo_actividades)
+    }
+}
