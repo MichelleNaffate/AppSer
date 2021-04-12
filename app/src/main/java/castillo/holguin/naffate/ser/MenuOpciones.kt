29b4477273078f05af33b71938967a5cdb3d478e
AnaCastilloMartinez <ana.castillo.itson@gmail.com>
2021-04-12 11:40:01 -0700

    Ajuste de detalles del diseño de las pantallas.

--- app/src/main/java/castillo/holguin/naffate/ser/MenuOpciones.kt
+++ app/src/main/java/castillo/holguin/naffate/ser/MenuOpciones.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class MenuOpciones : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,8 @@ class MenuOpciones : AppCompatActivity() {
         val button: Button = findViewById(R.id.btnActividades)
         val button2: Button = findViewById(R.id.btnRecomendaciones)
         val button3: Button = findViewById(R.id.btnHabitoTrabajar)
+        val button4: Button = findViewById(R.id.btnRitualMatutino)
+        val imagenUsuario: ImageView = findViewById(R.id.imageViewUsuario)
 
         button.setOnClickListener {
             var intent : Intent = Intent(this, CatalogoActividades:: class.java)
@@ -24,6 +27,16 @@ class MenuOpciones : AppCompatActivity() {
 
         button3.setOnClickListener {
             var intent : Intent = Intent(this, HabitoTrabajarActivity:: class.java)
+            startActivity(intent)
+        }
+
+        button4.setOnClickListener {
+            var intent : Intent = Intent(this, RitualMatutinoActivity:: class.java)
+            startActivity(intent)
+        }
+
+        imagenUsuario.setOnClickListener {
+            var intent : Intent = Intent(this, UsuarioActivity:: class.java)
             startActivity(intent)
         }
     }
