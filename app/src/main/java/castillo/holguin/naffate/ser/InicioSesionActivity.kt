7489dc87930c26214a09bfa85494132dd5698922
Daniel Holguin <39926374+MaxiNite@users.noreply.github.com>
2021-05-18 19:32:14 -0700

    Queda funcionalidad de RegistroActivity.kt y de InicioSesionActivity.kt por medio de Authentication con Firebase

--- app/src/main/java/castillo/holguin/naffate/ser/InicioSesionActivity.kt
+++ app/src/main/java/castillo/holguin/naffate/ser/InicioSesionActivity.kt
@@ -4,18 +4,59 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class InicioSesionActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_sesion)
-        val button: Button = findViewById(R.id.btnAcceder)
 
-        button.setOnClickListener {
-            var intent : Intent = Intent(this, MenuOpciones:: class.java)
-            startActivity(intent)
+        auth = Firebase.auth
 
+        val btn_acceder: Button = findViewById(R.id.btnAcceder)
 
+        btn_acceder.setOnClickListener {
+            valida_ingreso()
         }
+    }
+
+    private fun valida_ingreso(){
+        val et_correo: EditText = findViewById(R.id.edit_nombreUsuario)
+        val et_contra: EditText = findViewById(R.id.edit_contra)
+
+        var correo: String = et_correo.text.toString()
+        var contra: String = et_contra.text.toString()
+
+        if(!correo.isNullOrBlank() && !contra.isNullOrBlank()){
+
+            ingresarFirebase(correo,contra)
+        }else{
+            Toast.makeText(this, "Ingresar datos",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun ingresarFirebase(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+
+                    val intent:Intent = Intent(this,MenuOpciones::class.java)
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/castillo/holguin/naffate/ser/RegistroActivity.kt
+++ app/src/main/java/castillo/holguin/naffate/ser/RegistroActivity.kt
@@ -2,10 +2,67 @@ package castillo.holguin.naffate.ser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class RegistroActivity : AppCompatActivity() {
+
+    private lateinit var storage: FirebaseFirestore
+    private lateinit var usuario: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        usuario = Firebase.auth
+
+        val btn_registrar: Button = findViewById(R.id.btnRegistrarse)
+
+        btn_registrar.setOnClickListener {
+            valida_registro()
+        }
+    }
+
+    private fun valida_registro(){
+        val et_correo: EditText = findViewById(R.id.edit_emailNuevo)
+        val et_contra: EditText = findViewById(R.id.edit_contraNueva)
+        val et_contraConf: EditText = findViewById(R.id.edit_contraConfNueva)
+       // var et_nombUsuario: EditText = findViewById(R.id.edit_nombreUsuario)
+
+        //var usuario: String = et_nombUsuario.text.toString()
+
+        var correo: String = et_correo.text.toString()
+        var contra1: String = et_contra.text.toString()
+        var contra2: String = et_contraConf.text.toString()
+
+        if(!correo.isNullOrBlank() && !contra1.isNullOrBlank() && !contra2.isNullOrBlank()){
+            if(contra1 == contra2){
+                registrarFirebase(correo, contra1)
+            } else {
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, "Ingresar datos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun registrarFirebase(correo: String, password: String){
+        usuario.createUserWithEmailAndPassword(correo, password)
+            .addOnCompleteListener(this){  task ->
+                if (task.isSuccessful) {
+                    val user = usuario.currentUser
+
+                    Toast.makeText(baseContext, "${user?.email} Se ha creado correctamente.",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
